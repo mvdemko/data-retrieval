7ebb332b6235af0a21133fced3944b096a298f48
Maegen Demko <mvd326@gmail.com>
2025-07-18 12:43:15 -0700

    Removing logic for setting level in instance of tournament

--- src/data_retrieval/usta/tournaments/models/tournament.py
+++ src/data_retrieval/usta/tournaments/models/tournament.py
@@ -10,41 +10,9 @@ from data_retrieval.usta.tournaments.models.tournament_status import TournamentS
 
 
 class Tournament(BaseModel):
-    """
-    A class to represent a tennis event sponsored by USTA.
-
-    Attributes
-    ----------
-    name : str
-        The name of the tournament.
-    level : int
-        The level of the tournament. Level is a number between 1 and 7 that dictates how many
-        points are awarded to participants.
-    facility : Facility
-        An instance of the Facility class that describes where the event will occur.
-    status : str
-        Describes whether the tournament is still accepting registrations.
-    dates : Dates
-        An instance of the Dates class that describes when the event will occur.
-    events : list[Events]
-        A list of Event instances that provide more details on the events that make up the
-        tournament.
-
-    Methods
-    -------
-    __str__():
-        Print the tournament information.
-    """
-
     name: str  # TODO: Separate name and address
     url: str
     facility: Facility
     status: TournamentStatus
     dates: str  # TODO: Parse this and turn into a date object
     players: list[Player]
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        match = re.search(r"Level\s*(\d+)", self.name)
-        if match:
-            self.level = int(match.group(1))
