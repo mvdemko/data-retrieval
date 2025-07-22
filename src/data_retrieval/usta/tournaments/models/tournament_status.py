4a0407d9b9088421ed273db83f765f7218a68a27
Maegen Demko <mvd326@gmail.com>
2025-07-22 15:12:49 -0700

    Adding value to status enum

--- src/data_retrieval/usta/tournaments/models/tournament_status.py
+++ src/data_retrieval/usta/tournaments/models/tournament_status.py
@@ -4,4 +4,5 @@ from enum import Enum
 class TournamentStatus(Enum):
     REGISTRATIONS_OPEN = "Registrations open"
     REGISTRATIONS_CLOSED = "Registrations closed"
+    COMPLETED = "Completed"
     IN_PROGRESS = "In progress"
